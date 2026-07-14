1a329bb7d63ddd42fd71644414b3e9a0d8e0c058
Anapey01 <gabriel.k.anapey@gmail.com>
2026-07-14 15:01:15 +0000

    style(home): revert mobile process card and cta slide animation changes to original layout

--- js/home.js
+++ js/home.js
@@ -111,142 +111,150 @@ function initProcessCardAnimation() {
   function initAnimations() {
     ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
 
-    const isMobile = window.innerWidth < 1000;
-    const startWidth = isMobile ? 90 : 75;
-    const endWidth = isMobile ? 75 : 60;
+    const mm = gsap.matchMedia();
 
-    ScrollTrigger.create({
-      trigger: processSection,
-      start: "top top",
-      end: `+=${window.innerHeight * (isMobile ? 3 : 4)}px`,
-      scrub: 1,
-      pin: true,
-      pinSpacing: true,
-      onUpdate: (self) => {
-        const progress = self.progress;
+    mm.add("(max-width: 999px)", () => {
+      processCards.forEach((el) => (el.style = ""));
+      cardContainer.style = "";
+      stickyHeader.style = "";
+      return {};
+    });
 
-        if (progress >= 0.1 && progress <= 0.25) {
-          const headerProgress = gsap.utils.mapRange(
-            0.1,
-            0.25,
-            0,
-            1,
-            progress
-          );
-          const yValue = gsap.utils.mapRange(0, 1, 40, 0, headerProgress);
-          const opacityValue = gsap.utils.mapRange(
-            0,
-            1,
-            0,
-            1,
-            headerProgress
-          );
+    mm.add("(min-width: 1000px)", () => {
+      ScrollTrigger.create({
+        trigger: processSection,
+        start: "top top",
+        end: `+=${window.innerHeight * 4}px`,
+        scrub: 1,
+        pin: true,
+        pinSpacing: true,
+        onUpdate: (self) => {
+          const progress = self.progress;
 
-          gsap.set(stickyHeader, {
-            y: yValue,
-            opacity: opacityValue,
-          });
-        } else if (progress < 0.1) {
-          gsap.set(stickyHeader, {
-            y: 40,
-            opacity: 0,
-          });
-        } else if (progress > 0.25) {
-          gsap.set(stickyHeader, {
-            y: 0,
-            opacity: 1,
-          });
-        }
+          if (progress >= 0.1 && progress <= 0.25) {
+            const headerProgress = gsap.utils.mapRange(
+              0.1,
+              0.25,
+              0,
+              1,
+              progress
+            );
+            const yValue = gsap.utils.mapRange(0, 1, 40, 0, headerProgress);
+            const opacityValue = gsap.utils.mapRange(
+              0,
+              1,
+              0,
+              1,
+              headerProgress
+            );
 
-        if (progress <= 0.25) {
-          const widthPercentage = gsap.utils.mapRange(
-            0,
-            0.25,
-            startWidth,
-            endWidth,
-            progress
-          );
-          gsap.set(cardContainer, { width: `${widthPercentage}%` });
-        } else {
-          gsap.set(cardContainer, { width: `${endWidth}%` });
-        }
+            gsap.set(stickyHeader, {
+              y: yValue,
+              opacity: opacityValue,
+            });
+          } else if (progress < 0.1) {
+            gsap.set(stickyHeader, {
+              y: 40,
+              opacity: 0,
+            });
+          } else if (progress > 0.25) {
+            gsap.set(stickyHeader, {
+              y: 0,
+              opacity: 1,
+            });
+          }
 
-        if (progress >= 0.35 && !isGapAnimationCompleted) {
-          gsap.to(cardContainer, {
-            gap: isMobile ? "10px" : "20px",
-            duration: 0.5,
-            ease: "power3.out",
-          });
+          if (progress <= 0.25) {
+            const widthPercentage = gsap.utils.mapRange(
+              0,
+              0.25,
+              75,
+              60,
+              progress
+            );
+            gsap.set(cardContainer, { width: `${widthPercentage}%` });
+          } else {
+            gsap.set(cardContainer, { width: "60%" });
+          }
 
-          gsap.to([cardOne, cardTwo, cardThree], {
-            borderRadius: "20px",
-            duration: 0.5,
-            ease: "power3.out",
-          });
+          if (progress >= 0.35 && !isGapAnimationCompleted) {
+            gsap.to(cardContainer, {
+              gap: "20px",
+              duration: 0.5,
+              ease: "power3.out",
+            });
 
-          isGapAnimationCompleted = true;
-        } else if (progress < 0.35 && isGapAnimationCompleted) {
-          gsap.to(cardContainer, {
-            gap: "0px",
-            duration: 0.5,
-            ease: "power3.out",
-          });
+            gsap.to([cardOne, cardTwo, cardThree], {
+              borderRadius: "20px",
+              duration: 0.5,
+              ease: "power3.out",
+            });
 
-          gsap.to(cardOne, {
-            borderRadius: "20px 0 0 20px",
-            duration: 0.5,
-            ease: "power3.out",
-          });
+            isGapAnimationCompleted = true;
+          } else if (progress < 0.35 && isGapAnimationCompleted) {
+            gsap.to(cardContainer, {
+              gap: "0px",
+              duration: 0.5,
+              ease: "power3.out",
+            });
 
-          gsap.to(cardTwo, {
-            borderRadius: "0px",
-            duration: 0.5,
-            ease: "power3.out",
-          });
+            gsap.to(cardOne, {
+              borderRadius: "20px 0 0 20px",
+              duration: 0.5,
+              ease: "power3.out",
+            });
 
-          gsap.to(cardThree, {
-            borderRadius: "0 20px 20px 0",
-            duration: 0.5,
-            ease: "power3.out",
-          });
+            gsap.to(cardTwo, {
+              borderRadius: "0px",
+              duration: 0.5,
+              ease: "power3.out",
+            });
 
-          isGapAnimationCompleted = false;
-        }
+            gsap.to(cardThree, {
+              borderRadius: "0 20px 20px 0",
+              duration: 0.5,
+              ease: "power3.out",
+            });
 
-        if (progress >= 0.7 && !isFlipAnimationCompleted) {
-          gsap.to(processCards, {
-            rotationY: 180,
-            duration: 0.75,
-            ease: "power3.inOut",
-            stagger: 0.1,
-          });
+            isGapAnimationCompleted = false;
+          }
 
-          gsap.to([cardOne, cardThree], {
-            y: isMobile ? 15 : 30,
-            rotationZ: (i) => isMobile ? [-8, 8][i] : [-15, 15][i],
-            duration: 0.75,
-            ease: "power3.inOut",
-          });
+          if (progress >= 0.7 && !isFlipAnimationCompleted) {
+            gsap.to(processCards, {
+              rotationY: 180,
+              duration: 0.75,
+              ease: "power3.inOut",
+              stagger: 0.1,
+            });
 
-          isFlipAnimationCompleted = true;
-        } else if (progress < 0.7 && isFlipAnimationCompleted) {
-          gsap.to(processCards, {
-            rotationY: 0,
-            duration: 0.75,
-            ease: "power3.inOut",
-            stagger: -0.1,
-          });
+            gsap.to([cardOne, cardThree], {
+              y: 30,
+              rotationZ: (i) => [-15, 15][i],
+              duration: 0.75,
+              ease: "power3.inOut",
+            });
 
-          gsap.to([cardOne, cardThree], {
-            y: 0,
-            rotationZ: 0,
-            duration: 0.75,
-            ease: "power3.inOut",
-          });
+            isFlipAnimationCompleted = true;
+          } else if (progress < 0.7 && isFlipAnimationCompleted) {
+            gsap.to(processCards, {
+              rotationY: 0,
+              duration: 0.75,
+              ease: "power3.inOut",
+              stagger: -0.1,
+            });
 
-          isFlipAnimationCompleted = false;
-        }
-      },
+            gsap.to([cardOne, cardThree], {
+              y: 0,
+              rotationZ: 0,
+              duration: 0.75,
+              ease: "power3.inOut",
+            });
+
+            isFlipAnimationCompleted = false;
+          }
+        },
+      });
+      return () => {};
     });
   }
 
